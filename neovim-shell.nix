7e3d9e144683b9d82f4b94ab327db19563e43e77
Ion Sebastian Andrei <ionandrei5002@yahoo.com>
2020-01-27 22:44:25 +0200

    +modif

---
+++ neovim-shell.nix
@@ -0,0 +1,31 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  
+stdenv.mkDerivation rec {
+  name = "neovim-shell";
+  buildInputs = [
+    git
+    neovim
+    nodejs-12_x
+    yarn
+    nixpkgs.latest.rustChannels.stable.rust
+    rustracer
+    rustfmt
+    rustPlatform.rustcSrc
+    rls
+    cargo
+    pkgconfig
+    (
+      nixpkgs.python3.buildEnv.override rec {
+        extraLibs = with python3Packages; [
+          python3Packages.python-language-server
+        ];
+      }
+    )
+  ];
+  RUST_BACKTRACE  = 1;
+  RUST_SRC_PATH   = "${latest.rustChannels.stable.rust-src}/lib/rustlib/src/rust/src";
+}
