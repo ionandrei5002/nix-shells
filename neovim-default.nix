0afc719fed15c2c033c398f7c1f63e1a07c4da98
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2020-01-31 15:53:58 +0200

    +vifm

--- neovim-default.nix
+++ neovim-default.nix
@@ -4,5 +4,6 @@
   neovim  = import ./neovim-latest.nix { pkgs = import <nixpkgs-unstable> {}; };
   nodejs  = pkgs.nodejs-12_x;
   yarn    = pkgs.yarn;
+  vifm    = pkgs.vifm;
   #clion   = (import <nixpkgs-unstable> {}).jetbrains.clion;
 }
